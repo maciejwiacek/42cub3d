f5b219fdf064ead75d0026d800e83926f2d02d41
Maciej Wiacek <mwiacek@c3r16s1.42warsaw.pl>
2024-09-17 14:34:29 +0200

    Parsing done, todo: validation

--- src/map/parse_map.c
+++ src/map/parse_map.c
@@ -24,7 +24,8 @@ static char	*read_map(int fd)
 	map = ft_strdup("");
 	while (line)
 	{
-		map = ft_strjoin_free(map, line);
+		if (line[0] != '\n')
+			map = ft_strjoin_free(map, line);
 		line = get_next_line(fd);
 	}
 	free(line);
@@ -35,16 +36,14 @@ void	parse_map(t_game *game, char *av)
 {
 	int		map_fd;
 	char	*map;
-	// char	**map_split;
 
-	(void)game;
 	if (!check_extension(av, ".cub"))
 		print_error("Wrong map extension\n");
 	map_fd = open(av, O_RDONLY);
 	if (map_fd < 0)
 		print_error("Wrong map\n");
 	map = read_map(map_fd);
-	printf("%s\n", map);
-	// game->map = map;
+	game->map = ft_split(map, '\n');
+	free(map);
 	close(map_fd);
 }
