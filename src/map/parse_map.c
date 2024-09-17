b3e94997fd5a1a9974c4e20e692d3ca83ec7c2ad
Maciej Wiacek <mwiacek@c4r1s4.42warsaw.pl>
2024-09-17 22:39:22 +0200

    Some parsing

--- src/map/parse_map.c
+++ src/map/parse_map.c
@@ -35,17 +35,26 @@ static char	*read_map(int fd)
 static char	**process_map(t_game *game, char *map)
 {
 	char	**res;
+	int		i;
 
+	i = 0;
 	res = ft_split(map, '\n');
 	game->txt = parse_textures(game, res);
-	return (res);
+	while (i < 6)
+	{
+		free(res[i]);
+		i++;
+	}
+	return (&res[i]);
 }
 
 void	parse_map(t_game *game, char *av)
 {
 	int		map_fd;
 	char	*map;
+	int		i;
 
+	i = 0;
 	if (!check_extension(av, ".cub"))
 		print_error("Wrong map extension\n");
 	map_fd = open(av, O_RDONLY);
@@ -53,6 +62,10 @@ void	parse_map(t_game *game, char *av)
 		print_error("Wrong map\n");
 	map = read_map(map_fd);
 	game->map = process_map(game, map);
+	while (game->map[0][i] == ' ')
+		i++;
+	if (game->map[0][i] != '1')
+		print_error("Map format is wrong\n");
 	free(map);
 	close(map_fd);
 }
--- src/map/parse_textures.c
+++ src/map/parse_textures.c
@@ -7,7 +7,6 @@ t_img	*xpm_to_img(t_game *game, char *path)
 
 	img = malloc(sizeof(t_img));
 	fd = open(path, O_RDONLY);
-	printf("%s\n", path);
 	if (fd < 0)
 		print_error("Wrong texture path\n");
 	close (fd);
@@ -16,27 +15,51 @@ t_img	*xpm_to_img(t_game *game, char *path)
 	return (img);
 }
 
+static int	rgb_to_hex(char *color)
+{
+	int	r;
+	int	g;
+	int	b;
+
+	r = ft_atoi(color);
+	color = ft_strchr(color, ',') + 1;
+	g = ft_atoi(color);
+	color = ft_strchr(color, ',') + 1;
+	b = ft_atoi(color);
+	return ((r << 16) | (g << 8) | b);
+}
+
 t_txt	*parse_textures(t_game *game, char **map)
 {
 	int		i;
 	t_txt	*txt;
 	t_dict	txt_dict[6];
 
-	i = -1;
-	while (++i < 6)
+	i = 0;
+	while (map[i][0] && ft_isalpha(map[i][0]))
+	{
 		txt_dict[i] = to_dict(map[i]);
+		++i;
+	}
 	txt = malloc(sizeof(t_txt));
-	i = -1;
-	while (++i < 4)
+	i = 0;
+	while (map[i + 1][0] && map[i + 1][0] != '1')
 	{
 		if (!ft_strncmp(txt_dict[i].key, "NO", ft_strlen(txt_dict[i].key)))
 			txt->NO = xpm_to_img(game, txt_dict[i].value);
-		if (!ft_strncmp(txt_dict[i].key, "SO", ft_strlen(txt_dict[i].key)))
+		else if (!ft_strncmp(txt_dict[i].key, "SO", ft_strlen(txt_dict[i].key)))
 			txt->SO = xpm_to_img(game, txt_dict[i].value);
-		if (!ft_strncmp(txt_dict[i].key, "WE", ft_strlen(txt_dict[i].key)))
+		else if (!ft_strncmp(txt_dict[i].key, "WE", ft_strlen(txt_dict[i].key)))
 			txt->WE = xpm_to_img(game, txt_dict[i].value);
-		if (!ft_strncmp(txt_dict[i].key, "EA", ft_strlen(txt_dict[i].key)))
+		else if (!ft_strncmp(txt_dict[i].key, "EA", ft_strlen(txt_dict[i].key)))
 			txt->EA = xpm_to_img(game, txt_dict[i].value);
+		else if (!ft_strncmp(txt_dict[i].key, "F", ft_strlen(txt_dict[i].key)))
+			txt->F = rgb_to_hex(txt_dict[i].value);
+		else if (!ft_strncmp(txt_dict[i].key, "C", ft_strlen(txt_dict[i].key)))
+			txt->C = rgb_to_hex(txt_dict[i].value);
+		++i;
 	}
+	if (!(txt->NO && txt->SO && txt->WE && txt->EA && txt->F && txt->C))
+		print_error("Wrong textures\n");
 	return (txt);
 }
